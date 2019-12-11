e1116bd74665740a1813978f02ba55d6982198e0
lenovo <lenovo@LAPTOP-O7GUU6OB>
2019-12-11 23:54:45 +0530

    Spring Project Creation commit-'https://start.spring.io/' Dependencies: web,actuator,rest,devtools,security

---
+++ SpringProject/src/test/java/com/liveproject/SpringProject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveproject.SpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
